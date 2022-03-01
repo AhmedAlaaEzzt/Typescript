d67722b2c78d1f7a17fb630aaadb3093b8d95aeb
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-01 13:24:05 +0200

    00004 - prepare the demo

---
+++ index.ts
@@ -0,0 +1,23 @@
+let apples = 5;
+let speed = "fast";
+let hasName = true;
+let nothingMuch = null;
+let nothing = undefined;
+
+//built in objects
+let now = new Date();
+
+//Classes
+class Car {}
+let car = new Car();
+
+//Array
+let colors = ["red", "green", "blue"];
+let myNumbers = [1, 2, 3, 4];
+let truths = [true, true, false];
+
+//Object literal
+let point = {
+  x: 10,
+  y: 20,
+};
