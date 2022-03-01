e42289349ea292ea59f9912c4a10af40f1a7824c
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-01 08:33:05 +0200

    00003 - prepare demo

--- index.ts
+++ index.ts
@@ -2,15 +2,23 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-axios.get(url).then((response) => {
-  const todo = response.data;
+interface Todo{
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-  const id = todo.ID;
-  const title = todo.Title;
-  const completed = todo.Completed;
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
 
   console.log(`
     The Todo with Id: ${id}
     Has a title of: ${title}
     Is it completed: ${completed}`);
+
+
 });
