2dab19ba69b149aa53d61ffb31e15537e2b1aa0a
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-03 11:29:19 +0200

    00008 - prepare the demo

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-//in this example variable whose type cannot be inferred correctly
-let numbers = [-10, -1, 12];
-let numberAboveZero = false;
+const todayWeather = {
+  date: new Date(),
+  weather: "sunny",
+};
 
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > 0) {
-    numberAboveZero = numbers[i];
-  }
-}
+const logWeather = (forecast) => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+
+logWeather(todayWeather)
