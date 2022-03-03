640964b8d7f51d7c582b40fd7da8ba7283ca1fc4
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-03 09:12:13 +0200

    00007 - prepare the demo

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
-const json = '{"x":10, "y":20}';
-const coordinates = JSON.parse(json);
-console.log(coordinates);
+//in this example variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero = false;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
