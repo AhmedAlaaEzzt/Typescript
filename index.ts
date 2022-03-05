9db404bb5afe68b2b90d2127c634032b1e6a2777
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-05 10:31:15 +0200

    00012 - create the demo

--- index.ts
+++ index.ts
@@ -1,7 +1,1 @@
-const carMakers = ['ford', 'toyota', 'chevy'];
-const dates = [new Date(), new Date()];
-const carsByMake= [
-  ['f150'],
-  ['corolla'],
-  ['camaro']
-]
+const importantDates = [new Date(), '2030-10-10'];
