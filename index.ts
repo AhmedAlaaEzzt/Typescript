f5284859b5c59d64b668fb84aa538a4aeadd47d1
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-08 13:23:47 +0200

    00018 - create the demo

--- index.ts
+++ index.ts
@@ -2,18 +2,41 @@ interface Vehicle {
   name: string;
   year: number;
   broken: boolean;
+  summary(): string;
+}
+
+interface Drink {
+  color: string;
+  carbonated: boolean;
+  sugar: number;
+  summary(): string;
 }
 
 const oldCivic = {
   name: "civic",
   year: 2000,
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
+};
+
+const pepsi = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of suger`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+  console.log(vehicle.summary());
+};
+
+const printDrink = (drink: Drink): void => {
+  console.log(drink.summary());
 };
 
 printVehicle(oldCivic);
+printDrink(pepsi);
