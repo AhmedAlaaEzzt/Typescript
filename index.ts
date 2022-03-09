37ad3fcacbcfbdc3ebe9b25e35966181da1eff0e
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-09 08:16:26 +0200

    00022 - create the demo

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 class Vehicle {
-  color: string = "";
+  color: string;
+
+  constructor(color: string){
+    this.color = color;
+  }
 
   drive(): void {
     console.log("chugga chugga!");
@@ -10,7 +14,8 @@ class Vehicle {
   }
 }
 
-const vehicle = new Vehicle();
+const vehicle = new Vehicle("red");
 console.log("### Vehicle ###");
 vehicle.drive();
 vehicle.honk();
+console.log(vehicle.color);
