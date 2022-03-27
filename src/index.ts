bea37d7004be4662711228da8580fcecac29a2ba
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-27 12:27:54 +0200

    00027 - create the demo

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+/// <reference types="@types/google.maps" />
+
 import {User} from './User';
 import {Company} from './Company';
 console.log("hello there!");
@@ -8,3 +10,10 @@ const company1 = new Company();
 
 console.log(user1);
 console.log(company1);
+
+
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 3,
+    center: { lat: 0, lng: 0 },
+  });
+  
