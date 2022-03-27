80affe2c1af326bd88a2734d24528e9ca018b224
Ahmed Alaa <ahmed.alaa.ezzt@gmail.com>
2022-03-27 15:14:45 +0200

    00028 - create the demo

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 
 import {User} from './User';
 import {Company} from './Company';
+import { CustomMap } from "./CustomMap";
+
 console.log("hello there!");
 
 const user1 = new User();
@@ -12,8 +14,4 @@ console.log(user1);
 console.log(company1);
 
 
-new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    zoom: 3,
-    center: { lat: 0, lng: 0 },
-  });
-  
+new CustomMap('map');
